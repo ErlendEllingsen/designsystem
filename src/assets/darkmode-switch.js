95a00bcd6b8703447bf43cfee3f042e6956ae824
Pål Oliver Kristiansen <paal.oliver.kristiansen@sparebank1.no>
2019-12-12 13:19:28 +0100

    feat: switche mellom light/dark mode (for ferdig bygget styleguide)

---
+++ src/assets/darkmode-switch.js
@@ -0,0 +1,73 @@
+var darkModeEnabled = false;
+
+function isDarkModeSupported() {
+    if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
+        return true;
+    }
+
+    return false;
+}
+
+function onDarkModeSelected() {
+    document.body.classList.add('native');
+    document.querySelectorAll('.darkmode-switch-icon').forEach(icon => {
+        icon.style.filter = 'invert(100%)';
+    });
+}
+
+function onLightModeSelected() {
+    document.body.classList.remove('native');
+    document.querySelectorAll('.darkmode-switch-icon').forEach(icon => {
+        icon.style.filter = '';
+    });
+}
+
+var checkbox = document.getElementById('darkmode-switch');
+
+let animateCssTransition = () => {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('transition');
+    }, 1000);
+};
+
+checkbox.addEventListener('click', function(event) {
+    if (!isDarkModeSupported()) {
+        alert('Sorry! Darkmode is not supported with this browser.');
+        event.preventDefault();
+        return;
+    }
+});
+
+checkbox.addEventListener('change', function() {
+    if (this.checked) {
+        darkModeEnabled = true;
+        animateCssTransition();
+        onDarkModeSelected();
+        document.querySelector("link[href='styles.css']").href =
+            'styles-darkmode.css';
+    } else {
+        darkModeEnabled = false;
+        animateCssTransition();
+        onLightModeSelected();
+        document.querySelector("link[href='styles-darkmode.css']").href =
+            'styles.css';
+    }
+});
+
+var mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+mediaQueryList.addEventListener('change', () => {
+    var darkModeEndabledByOS = window.matchMedia('(prefers-color-scheme: dark)')
+        .matches;
+    var labelEl = document.getElementById('switch-label');
+
+    // disable manual swith if dark mode is enabled OS-wide
+    checkbox.checked = darkModeEndabledByOS;
+    darkModeEndabledByOS
+        ? labelEl.classList.add('inputDisabled')
+        : labelEl.classList.remove('inputDisabled');
+    darkModeEndabledByOS
+        ? (checkbox.disabled = true)
+        : (checkbox.disabled = false);
+    darkModeEndabledByOS ? onDarkModeSelected() : onLightModeSelected();
+});
